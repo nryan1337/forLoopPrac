0fb3bf1fdb75bc8fa5fd2f2d02dc4d6ea80b8d5d
Nic Ryan <nryan@timelio.net.au>
2017-05-30 10:45:18 +1000

    added form validation

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,32 @@ $(document).ready(function() {
     number1 = parseInt(number1);
     number2 = parseInt(number2);
 
-    // array to capture result of index through iterations of the loop
-    var output = [];
+    if (isNaN(number1) || isNaN(number2)) {
+      alert("please enter a number in both inputs");
+    }
+    else if (number2 > number1) {
+       alert("the 'by' number needs to be less than the 'count to' number");
+    }
+    else {
+      // array to capture result of index through iterations of the loop
+      var output = [];
 
-    for (var index = number2; index <= number1; index+=number2) {
-      output.push(index);
+      for (var index = number2; index <= number1; index+=number2) {
+        output.push(index);
+      }
+
+
+
+      var result = output.join();
+
+      $("#countUpByOutput").show();
+      $("#number1Output").text(number1);
+      $("#number2Output").text(number2);
+      $("#resultOutput").text(result + "\n");
+
+      event.preventDefault();
     }
 
-    var result = output.join();
 
-    $("#countUpByOutput").show();
-    $("#number1Output").text(number1);
-    $("#number2Output").text(number2);
-    $("#resultOutput").text(result + "\n");
-
-    event.preventDefault();
   });
 });
